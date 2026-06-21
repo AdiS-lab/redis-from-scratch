0cf3e01f96be73c47ed5a7e480a23bc009d0a53c
Adi Shankar <adishankar219@gmail.com>
2026-06-21 11:45:29 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -11,30 +11,28 @@ var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment the code below to pass the first stage
 	
+	//__________________________ intialize TCP connection _____________________________
 	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+	//___________________________ read from multiple clients ___________________________
 
-	conn, err := listener.Accept()
-	if err!= nil{ 
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-
-	buf := make([]byte, 1024)  
 	for {
-		_,err := conn.Read(buf)
-		if err != nil{
-			break
+		conn, err := listener.Accept()
+		if err!= nil{ 
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
 		}
-	conn.Write([]byte("+PONG\r\n"))
+		buf := make([]byte, 1024)  
+		for {
+			_,err := conn.Read(buf)
+			if err != nil{
+				break
+			}
+		conn.Write([]byte("+PONG\r\n"))
+		}
 	}
-
 }
