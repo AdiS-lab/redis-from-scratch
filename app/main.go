f2a73d287845b929c52dac85987bb7cbca87ce7d
Adi Shankar <adishankar219@gmail.com>
2026-06-21 14:32:55 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -72,6 +72,7 @@ func handleRealConnection(reader *bufio.Reader, conn net.Conn, count int, initia
 		name := make([]byte, int(n - '0')) // create a buffer to hold the new data 
 		reader.Read(name)
 
+		fmt.Println(string(name))
 		statement = append(statement, string(name))
 
 		reader.ReadByte() // bypass the last /r/n
