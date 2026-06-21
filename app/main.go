9749816e75d12114d7c01c690edb61dd97dcbdf8
Adi Shankar <adishankar219@gmail.com>
2026-06-21 11:54:18 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -14,7 +14,7 @@ func main() {
 	
 	//__________________________ intialize TCP connection _____________________________
 	for{
-		listener, err := net.Listen("tcp", "0.0.0.0:6379")
+		listener, err := net.Listen("tcp", "127.0.0.1:6379")
 		if err != nil {
 			fmt.Println("Failed to bind to port 6379")
 			os.Exit(1)
