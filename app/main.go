9035777ea60f680853e2feeb0ae4311c533403a5
Adi Shankar <adishankar219@gmail.com>
2026-06-21 11:57:50 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -14,13 +14,12 @@ func main() {
 
 	//__________________________ intialize TCP connection _____________________________
 	listener, err := net.Listen("tcp", "0.0.0.0:6379")
-		if err != nil {
-			fmt.Println("Failed to bind to port 6379")
-			os.Exit(1)
-		}
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
+
 	for{
-	//___________________________ read from multiple clients ___________________________
-		fmt.Println(listener)
 		conn, err := listener.Accept()
 		if err!= nil{ 
 			fmt.Println("Error accepting connection: ", err.Error())
@@ -33,7 +32,6 @@ func main() {
 			if err != nil{
 				break
 			}
-		fmt.Println("made it inside the loop")
 		conn.Write([]byte("+PONG\r\n"))
 		break
 		}
