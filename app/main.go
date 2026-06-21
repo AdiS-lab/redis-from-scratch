4f305ebef2b1c1760e0b2b0740fc2e986937e472
Adi Shankar <adishankar219@gmail.com>
2026-06-21 15:22:23 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -54,7 +54,7 @@ func handleConnection(conn net.Conn){ //  conn is a byte slice
 		case "SET":
 			if strings.ToUpper(statement[3]) == "PX"{ //  checking if they added expiry date. 
 				storage[statement[1]] = statement[2]
-				ms := int(statement[5])
+				ms := strings.ParseInt(statement[5])
 				ticker := time.NewTicker(ms * (time.Second/1000))
 				for range ticker.C{
 					delete(storage, statement[1])
