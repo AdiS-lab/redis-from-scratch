db250260255cafac7a8112659476fb0ec9a8815b
Adi Shankar <adishankar219@gmail.com>
2026-06-22 08:22:40 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -167,7 +167,7 @@ func handleConnection(conn net.Conn) { //  conn is a byte slice
 				storage[storageKey] = "1"
 				conn.Write([]byte(":1\r\n"))
 			}else if (reflect.TypeOf(storage[storageKey]) != nil){
-				fmt.Println(value)
+				fmt.Println(reflect.TypeOf(value))
 			}else{
 			// }else if(reflect.TypeOf(lists[listName]) != "int"){
 			// 	conn.Write([]byte("+-1\r\n"))
