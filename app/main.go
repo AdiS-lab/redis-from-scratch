5492689c019f1db5f9202b120826149decbe1998
Adi Shankar <adishankar219@gmail.com>
2026-06-22 12:18:23 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -53,8 +53,11 @@ func handleConnection(conn net.Conn) { //  conn is a byte slice
 
 		if (isQueue == true && len(statement)>0 && input != "EXEC"){
 			queue = append(queue, statement)
-			fmt.Println("made it to QUEUED")
+			if(input == "GET"){
+				conn.Write([]byte("$-1\r\n"))
+			}else{
 			conn.Write([]byte("+QUEUED\r\n"))
+			}
 		
 		}else if (isQueue == true && len(statement)>0 && input == "EXEC"){
 			isQueue = false
