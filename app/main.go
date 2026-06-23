fa5b5f279aa77316f9fd6803d26bd9cb7080854c
Adi Shankar <adishankar219@gmail.com>
2026-06-23 12:03:20 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -343,7 +343,8 @@ func execute(statement []string, conn net.Conn, fullPort string) string {
 		return "*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n"
 	case "REPLCONF": 
 		if(len(statement)>2 && statement[1] == "GETACK"){
-			return "*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$1\r\n0\r\n"
+			offset := data["master_repl_offset"]
+			return fmt.Sprintf("*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$%d\r\n%s\r\n", len(offset), offset)
 		}
 		return "+OK\r\n"
 	default:
