d63b5f2f1c68d814cfa892ee7ae97cba790692df
Adi Shankar <adishankar219@gmail.com>
2026-06-22 19:14:36 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -303,12 +303,11 @@ func execute(statement []string ,conn net.Conn, fullPort string) string{
 			if firstOK == false{
 				firstOK = true
 				return ("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n")
-			}else if(firstOK == true){
-				return "*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n"
 			}
-			return "" // case where 
-		case "PSYNC":
-			return ""
+
+			return "*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n"
+		// case "PSYNC":
+		// 	return ""
 		default:
 			return ("+messageNotFound\r\n")
 		}
