47e04c8eafd18236ffc962b5f827d2874f0a7411
Adi Shankar <adishankar219@gmail.com>
2026-06-24 14:57:02 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -84,6 +84,7 @@ func handleConnection(conn net.Conn, fullPort string) { //  conn is a byte slice
 			// result, _ := os.ReadFile(configs["manifest"]) //  use manifest to identify 
 			fmt.Println("made it to the append stage ")
 			targetFile := filepath.Join(configs["dirname"], fmt.Sprintf("%s.1.incr.aof", configs["filename"])) // find targetFile string 
+			fmt.Println("this is target file ", targetFile)
 			file, _ := os.OpenFile(targetFile, os.O_APPEND, 0644)
 			file.WriteString(recreatedCmd)
 			file.Close()
