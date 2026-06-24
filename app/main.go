6f542c0ed93be114ecff0ae8cfae31acc91e3b6d
Adi Shankar <adishankar219@gmail.com>
2026-06-23 21:36:29 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -390,9 +390,9 @@ func execute(statement []string, conn net.Conn, fullPort string) string {
 			directory := configs["dir"] 
 			filePath := configs["dbfilename"]
 			info,_ := os.ReadFile(filePath)
-			readRDB(info)
 			fullPath := directory + filePath
 			fmt.Println("this the entire path " + fullPath)
+			readRDB(info)
 			// fullPath,_ := filepath.Join(directory, filePath)
 			fmt.Println(string(info))
 			if info == nil{
