86dbbf0430e188b280f8bb3c06ecd461f3dbb2d3
Adi Shankar <adishankar219@gmail.com>
2026-06-25 10:08:50 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -628,7 +628,7 @@ func execute(statement []string, conn net.Conn, fullPort string) string {
 		longitude,_ := strconv.ParseFloat(statement[2], 64)
 		latitude,_ := strconv.ParseFloat(statement[3], 64)
 		if !(longitude >= -180 && longitude <= 180) || !(latitude >= -85.05112878 && latitude <= 85.05112878){
-			return fmt.Sprintf("-ERR invalid longitude,latitude pair %f, %f", longitude, latitude)
+			return fmt.Sprintf("-ERR invalid longitude,latitude pair %f, %f\r\n", longitude, latitude)
 		}
 		return ":1\r\n"
 	default:
